0f714cd7c60c7892622f67791a3c8564410149f9
pratt3000 <pratt3000@gmail.com>
2020-05-28 17:16:12 +0530

    paddle size adjusted

--- pong.py
+++ pong.py
@@ -17,7 +17,7 @@ paddle_a = turtle.Turtle()
 paddle_a.speed(0)               #set speed to max
 paddle_a.shape("square")
 paddle_a.color("white")
-paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.shapesize(stretch_wid=7, stretch_len=0.5)
 paddle_a.penup()
 paddle_a.goto(-350, 0)
 
@@ -26,7 +26,7 @@ paddle_b = turtle.Turtle()
 paddle_b.speed(0)               #set speed to max
 paddle_b.shape("square")
 paddle_b.color("white")
-paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.shapesize(stretch_wid=7, stretch_len=0.5)
 paddle_b.penup()
 paddle_b.goto(350, 0)
 
@@ -129,12 +129,12 @@ while True:
 
 
     #Paddle collision with ball and rebound
-    if (ball.xcor() > 340)and (ball.xcor()<350) and (ball.ycor() < paddle_b.ycor() + 40) and (ball.ycor() > paddle_b.ycor() - 40):
+    if (ball.xcor() > 340)and (ball.xcor()<350) and (ball.ycor() < paddle_b.ycor() + 60) and (ball.ycor() > paddle_b.ycor() - 60):
         ball.setx(340)
         ball.dx *= -1
         os.system("aplay bounce.wav&")
 
-    if (ball.xcor() < -340)and (ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 40) and (ball.ycor() > paddle_a.ycor() - 40):
+    if (ball.xcor() < -340)and (ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 60) and (ball.ycor() > paddle_a.ycor() - 60):
         ball.setx(-340)
         ball.dx *= -1
         os.system("aplay bounce.wav&")
